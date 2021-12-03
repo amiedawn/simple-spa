adaee8b70b204e18b3257121d93c9c9821dc111b
amiedawn <amie.d.carroll@gmail.com>
2021-12-02 19:52:52 -0700

    add username component

--- src/components/Username.js
+++ src/components/Username.js
@@ -1,13 +1,28 @@
 import React, { useState } from 'react';
 
 function Username(props) {
-  const [name, setName] = useState();
+  const [name, setName] = useState("");
+  const [newName, setNewName] = useState("");
+
 
   return (
     <div>
-      Username: <input type="text" name="username" id="username" />
-      <input type="submit" className="btn" value="Submit" />
-      <p>Welcome {name}!</p>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          setNewName((e) => name);
+        }}
+      >
+        Username:{" "}
+        <input
+          type="text"
+          name="username"
+          id="username"
+          onChange={(event) => setName((e) => event.target.value)}
+        />
+        <input type="submit" className="btn" value="Submit" />
+        <p>Welcome {newName}!</p>
+      </form>
     </div>
   );
 }
